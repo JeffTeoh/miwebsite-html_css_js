1bc6ab83022483b05ce476f0953f0024f8fb0537
JeffTeoh <jeffrey@gutstudio.com>
2018-03-26 15:25:39 +0800

    Fix the video that still playing after closing modal dialog

--- miwebsite.js
+++ miwebsite.js
@@ -10,5 +10,56 @@ $(document).ready(function(){
 	});
 });
 
-
+		/*$('.dropdown').hover(function(){
+			$('.dropdown-toggle', this).trigger('click');
+		});*/
+		
+		/*var hoverTimeout;
+		$('.dropdown').hover(function(){
+			clearTimeout(hoverTimeout);
+			$('.dropdown-toggle', this).trigger('click');
+		}, function(){
+			var $self = $(this);
+			hoverTimeout = setTimeout(function(){
+				$self.removeClass('open');
+			}, 150);
+		});*/
+		
+		/*$('.dropdown-menu')
+		.mouseover(function(){
+			$(this).addClass.('show');
+		})
+		.mouseout(function(){
+			$(this).removeClass('show');
+		});*/
+		
+		/*$('body').on('mouseenter mouseleave','.dropdown',function(e){
+			var _d=$(e.target).closest('.dropdown');_d.addClass('show');
+			setTimeout(function(){
+				_d[_d.is(':hover')?'addClass':'removeClass']('show');
+			},300);
+		});*/
+		
+		
+		
+		/*$(function(){
+			$('#modalMiMax2').modal({
+				show:false
+			}).on('hidden.bs.modal', function () {
+				$(this).find('video')[0].pause();
+			});
+		});*/
+		
+		/*$(function(){
+			$('.launchVideo').on('click', function(e){
+				e.preventDefault();
+				$( '#' + $(this).data('modal-id') ).modal();
+			}); 
+			// reload the modal contents when it is closed
+			$("#modal-video").on("hidden.bs.modal", function () {
+				var url = $('#video-frame').attr('src');
+				$('#video-frame').attr('src', '');
+				$('#video-frame').attr('src', url);
+			});
+		});*/
 
